c2ec3807c1bedb185fc175cefface937b89d838b
Lili <llxiao.design@gmail.com>
2024-10-04 21:18:39 +0100

    Improved version for awesome garage

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -14,7 +14,7 @@ class FavouritesController < ApplicationController
     @car = Car.find(params[:car_id])
     @favourite.car = @car
     if @favourite.save
-      redirect_to car_path(@car), notice: "Favourite was successfully created."
+      redirect_to car_path(@car), notice: "Car was successfully added to your garage."
     else
       render "cars/show", status: :unprocessable_entity
     end
